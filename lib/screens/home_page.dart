52d99a0961e3252f607727e0181c8135b915117b
ShanusanT <tamilmotive@gmail.com>
2024-06-01 19:54:59 +0530

    Product details page completed

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:prodigit_test/models/product.dart';
+import 'package:prodigit_test/screens/product_details_page.dart';
 import 'package:prodigit_test/services/api_service.dart';
 import 'package:prodigit_test/utils/colors.dart';
 
@@ -26,8 +27,9 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         backgroundColor: backgroundColor,
         surfaceTintColor: backgroundColor,
-        title: const Text(
-            'Product List',
+        title:  Text(
+            "Product List",
+          style: Theme.of(context).textTheme.headlineMedium,
         ),
         centerTitle: true,
       ),
@@ -48,39 +50,43 @@ class _HomePageState extends State<HomePage> {
                 itemCount: snapshot.data!.length,
                 itemBuilder: (context, index) {
                   final product = snapshot.data![index];
-                  return Container(
-                    decoration: BoxDecoration(
-                      color: secondaryBackgroundColor,
-                      borderRadius: BorderRadius.circular(16),
-                    ),
-                    child: SingleChildScrollView(
-
-                      child: Column(
-                        children: [
-                          Image.network(product.thumbnail,fit: BoxFit.contain,height: 150,),
-
-                          ListTile(
-                            title: Text(product.title,maxLines: 2,overflow: TextOverflow.ellipsis,),
-                            subtitle: Text(product.brand, maxLines: 1, overflow: TextOverflow.ellipsis,),
-                            //trailing: Text('\$${product.price}'),
-                          ),
-
-                          const SizedBox(height: 8,),
-
-                          Padding(
-                            padding: const EdgeInsets.only(left: 16.0),
-                            child: Align(
-                              alignment: Alignment.centerLeft,
-                              child: Text(
-                                'Rs. ${product.price}',
-                                style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                                  color: brandColor,
-                                  fontWeight: FontWeight.bold
-                                )
+                  return InkWell(
+                    onTap: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (BuildContext context)=>ProductDetailsPage(product: product)));
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: secondaryBackgroundColor,
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      child: SingleChildScrollView(
+                    
+                        child: Column(
+                          children: [
+                            Image.network(product.thumbnail,fit: BoxFit.contain,height: 150,),
+                    
+                            ListTile(
+                              title: Text(product.title,maxLines: 2,overflow: TextOverflow.ellipsis,),
+                              subtitle: Text(product.brand, maxLines: 1, overflow: TextOverflow.ellipsis,),
+                            ),
+                    
+                            const SizedBox(height: 8,),
+                    
+                            Padding(
+                              padding: const EdgeInsets.only(left: 16.0),
+                              child: Align(
+                                alignment: Alignment.centerLeft,
+                                child: Text(
+                                  'Rs. ${product.price}',
+                                  style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                                    color: brandColor,
+                                    fontWeight: FontWeight.bold
+                                  )
+                                ),
                               ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
                   );
---
+++ lib/screens/product_details_page.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:prodigit_test/models/product.dart';
+import 'package:prodigit_test/utils/colors.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+
+class ProductDetailsPage extends StatefulWidget {
+  const ProductDetailsPage({super.key, required this.product});
+
+  final Product product;
+
+  @override
+  State<ProductDetailsPage> createState() => _ProductDetailsPageState();
+}
+
+class _ProductDetailsPageState extends State<ProductDetailsPage> {
+
+
+  int _currentIndex = 0;
+  double averageRating = 0;
+  final CarouselController _carouselController = CarouselController();
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.product.reviews.isNotEmpty) {
+      averageRating = widget.product.reviews.map((review) => review.rating).reduce((a, b) => a + b) / widget.product.reviews.length;
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+
+
+
+    return Scaffold(
+      backgroundColor: secondaryBackgroundColor,
+      appBar: AppBar(
+        title: Text("Product Details",
+          style: Theme.of(context).textTheme.headlineMedium,
+        ),
+        backgroundColor: secondaryBackgroundColor,
+        surfaceTintColor: secondaryBackgroundColor,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            CarouselSlider(
+              carouselController: _carouselController,
+              options: CarouselOptions(
+                  height: 400.0,
+                autoPlay: true,
+                onPageChanged: (index, reason){
+                    setState(() {
+                      _currentIndex = index;
+                    });
+                }
+              ),
+              items: widget.product.images.map((image) {
+                return Builder(
+                  builder: (BuildContext context) {
+                    if(widget.product.images.isEmpty){
+                      return Image.asset("assets/images/placeholder.png");
+                    }
+                    return Image.network(image,fit: BoxFit.contain,);
+                  },
+                );
+              }).toList(),
+            ),
+            const SizedBox(height: 16.0),
+            Center(
+              child: AnimatedSmoothIndicator(
+                  activeIndex: _currentIndex,
+                  count: widget.product.images.length,
+                axisDirection: Axis.horizontal,
+                effect: const ScrollingDotsEffect(
+                  dotColor: Colors.grey,
+                  activeDotColor: brandColor,
+                  dotHeight: 8.0,
+                  dotWidth: 8.0,
+                ),
+                onDotClicked: (index){
+                    _carouselController.animateToPage(index);
+                },
+              ),
+            ),
+
+            const SizedBox(height: 16.0),
+
+
+            const SizedBox(height: 8.0),
+            ListTile(
+              title: Text(
+                widget.product.title,
+                textAlign: TextAlign.left,
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                style: Theme.of(context).textTheme.headlineMedium
+              ),
+              trailing: Text(
+                  'Rs. ${widget.product.price}',
+                style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                  color: brandColor,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+            ),
+
+
+             Padding(
+               padding: const EdgeInsets.only(left: 16.0,right: 16.0),
+               child: Row(
+                 mainAxisAlignment: MainAxisAlignment.start,
+                 children: [
+                   Text(
+                       'Rating: ${averageRating.toStringAsFixed(1)}',
+                       style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                           color: Colors.black54,
+                       )
+                   ),
+
+
+                   RatingBarIndicator(
+                     rating: averageRating,
+                     direction: Axis.horizontal,
+                     itemCount: 5,
+                     itemSize: 18,
+                     unratedColor: Colors.black54,
+                     itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
+                     itemBuilder: (context, _) => const Icon(
+                       Icons.star,
+                       color: Colors.amber,
+                     ),
+                   ),
+                 ],
+               ),
+             ),
+
+            const SizedBox(height: 16.0),
+
+            Padding(
+              padding: const EdgeInsets.only(left: 16.0,right: 16.0),
+              child: Text(
+                widget.product.description,
+                textAlign: TextAlign.start,
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
